a4e50719dfb499ecf0abd3aed363ef7714d2b3b3
gonzaabenitezz <gonzaabenitezz@gmial.com>
2025-03-07 18:46:41 -0300

    Agregar archivos iniciales del proyecto del consultorio de Odontologia

---
+++ target/generated-sources/annotations/logica/Responsable_.java
@@ -0,0 +1,13 @@
+package logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2025-03-07T18:39:02")
+@StaticMetamodel(Responsable.class)
+public class Responsable_ extends Persona_ {
+
+    public static volatile SingularAttribute<Responsable, String> tipo_resp;
+
+}
---
+++ target/generated-sources/annotations/logica/Usuario_.java
@@ -0,0 +1,16 @@
+package logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2025-03-07T18:39:02")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, Integer> id_usuario;
+    public static volatile SingularAttribute<Usuario, String> contrasenia;
+    public static volatile SingularAttribute<Usuario, String> nombreUsuario;
+    public static volatile SingularAttribute<Usuario, String> rol;
+
+}
